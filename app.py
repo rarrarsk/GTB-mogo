98159af5c0cfb1c063c228cca2f2734206ac572e
rarrarsk <125333263+rarrarsk@users.noreply.github.com>
2023-04-14 12:52:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://rarrarsk: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@github.com/rarrarsk/GTB ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
